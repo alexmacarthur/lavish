9db196537d8c245d0b51d24dd720087a74da2f37
Alex MacArthur <alex@macarthur.me>
2016-05-31 22:42:21 -0500

    Working through page layout.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,9 @@ var Lavish = {
 		$('#testimonialSlider').slick({
 			infinite: true,
 			slidesToShow: 3,
-			slidesToScroll: 3
+			slidesToScroll: 3, 
+			prevArrow: $('#slickPrev'),
+			nextArrow: $('#slickNext')
 		});
 	}
 }
