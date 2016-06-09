23723d9efe2d28f2d0b9ed49b58e91270424d154
Alex MacArthur <alex@macarthur.me>
2016-06-08 23:02:03 -0500

    Progress on contact secact section.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,7 +12,38 @@ var Lavish = {
 			prevArrow: $('#slickPrev'),
 			nextArrow: $('#slickNext')
 		});
+	}, 
+
+	contactForm : function() {
+		$('#ContactForm').on('submit', function(e){
+			e.preventDefault();
+
+			var $formName = $('#formName');
+			var $formEmail = $('#formEmail');
+			var $formMessage = $('#formMessage');
+			var $statusMessages = $('#StatusMessages');
+
+			$statusMessages.removeClass('failure success');
+
+			$.ajax({
+				url: "//formspree.io/alex@macarthur.me",
+				method: "POST",
+				data: {
+					name: $formName.val(),
+					email: $formEmail.val(),
+					message: $formMessage.val(),
+				},
+				dataType: "json"
+			}).done(function(response) {
+				$formName.val('');
+				$formEmail.val('');
+				$formMessage.val('');
+				$statusMessages.html('Your message was successfully sent! Thanks.').removeClass('failure').addClass('success');
+			}).fail(function(data) {
+				$statusMessages.html('Sorry, an something\'s messed up. Refresh the page to try again, or just send an email to alex@macarthur.me.').removeClass('success').addClass('failure');
+			});
+		});
 	}
-}
+};
 
 Lavish.init();
