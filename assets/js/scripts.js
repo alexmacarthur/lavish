079fe587b969afa7b0f43bf0c7764d335e7b3db8
Alex MacArthur <alex.macarthur@daveramsey.com>
2016-06-13 13:24:17 -0500

    SVG drawing working...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,25 @@ var Lavish = {
 
 	init : function() {
 		this.slider();
+		this.contactForm();
+	},
+
+	svgDividers : {
+		0 : {
+			name : 'Mirror',
+			beenDrawn : false
+		}, 
+
+		1 : {
+			name : 'Scissors',
+			beenDrawn : false
+		}, 
+
+		2 : {
+			name: 'Shampoo',
+			beenDrawn : false
+		}
+
 	},
 
 	slider : function() {
@@ -13,6 +32,10 @@ var Lavish = {
 			nextArrow: $('#slickNext')
 		});
 	}, 
+
+	drawSVG : function(el, side) {
+  		new Vivus(el, {duration: 50, file: 'assets/img/svg-swirl-' + side + '.svg'}, null);
+	},
 
 	contactForm : function() {
 		$('#ContactForm').on('submit', function(e){
@@ -45,10 +68,58 @@ var Lavish = {
 		});
 	},
 
-	parallax : function(){
+	parallax : function($el){
 	  	var scrolled = $(window).scrollTop();
 	  	$('.Background').css('top',-(scrolled*0.1)+'px');
-	}
+	}, 
+
+	_isVisible: function($el) {
+      var win = $(window);
+
+      var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+      };
+
+      viewport.right = viewport.left + win.width();
+      viewport.bottom = viewport.top + win.height();
+
+      var height = $el.outerHeight();
+      var width = $el.outerWidth();
+
+      if (!width || !height) {
+        return false;
+      }
+
+      var bounds = $el.offset();
+      bounds.right = bounds.left + width;
+      bounds.bottom = bounds.top + height;
+
+      var visible = (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+      if (!visible) {
+        return false;
+      }
+
+      var deltas = {
+        top: Math.min(1, (bounds.bottom - viewport.top) / height),
+        bottom: Math.min(1, (viewport.bottom - bounds.top) / height),
+        left: Math.min(1, (bounds.right - viewport.left) / width),
+        right: Math.min(1, (viewport.right - bounds.left) / width)
+      };
+
+      return (deltas.left * deltas.right) >= 1 && (deltas.top * deltas.bottom) >= 1;
+    },
+
+    svgChecks : function() {
+    	Object.keys(Lavish.svgDividers).forEach(function(key) {
+			if(Lavish._isVisible($('#divider' + Lavish.svgDividers[key]['name'])) && !Lavish.svgDividers[key]['beenDrawn']) {
+				Lavish.svgDividers[key]['beenDrawn'] = true;
+				Lavish.drawSVG('svg' + Lavish.svgDividers[key]['name'] + 'Left', 'left');
+				Lavish.drawSVG('svg' + Lavish.svgDividers[key]['name'] + 'Right', 'right');
+			}	
+	    });
+    }
 };
 
 $(document).ready(function() {
@@ -56,5 +127,9 @@ $(document).ready(function() {
 
 	$(window).scroll(function(e){
 	 	Lavish.parallax();
+	 	Lavish.svgChecks();
 	});
+
+	Lavish.svgChecks();
+
 });
