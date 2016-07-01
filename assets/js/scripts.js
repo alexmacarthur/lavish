42e63ce7317d571cefeea0d68d382e5a903d9fa8
Alex MacArthur <alex.macarthur@daveramsey.com>
2016-07-01 15:59:09 -0500

    making responsive.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,7 +40,23 @@ var Lavish = {
 			slidesToShow: 3,
 			slidesToScroll: 3, 
 			prevArrow: $('#slickPrev'),
-			nextArrow: $('#slickNext')
+			nextArrow: $('#slickNext'),
+			responsive: [
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    },
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+		  	]
 		});
 	}, 
 
