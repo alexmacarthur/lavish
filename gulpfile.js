a1c8a8c8cdd2948a23c72bea5edb0ac3b82be09f
Alex MacArthur <alex@macarthur.me>
2016-09-02 20:41:02 -0500

    Inline styles.

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('sass',function(){
   gulp.src('assets/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefix('last 3 versions'))
-    .pipe(gulp.dest('assets'));
+    .pipe(gulp.dest('_includes'));
 });
 
 gulp.task('watch', function() {
